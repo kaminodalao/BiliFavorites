0012544a73e6f275a827aa275f3a3707c2911d76
kamino <kamino@imea.me>
2022-01-08 16:23:00 +0800

    debug

--- update_video.py
+++ update_video.py
@@ -194,9 +194,9 @@ class UpdateVideoStarList:
                     cover = qi.backup
                 else:
                     cover = ""
-                details_tmp.append(
+                """details_tmp.append(
                     f'<div align="center"><img alt="{video.title}" src="{cover}" width="50%" /></div>  '
-                )
+                )"""
                 details_tmp.append("Title: %s  " % video.title)
                 details_tmp.append("Intro: %s  " % video.intro)
                 details_tmp.append("Category: %s  " % category.title)
